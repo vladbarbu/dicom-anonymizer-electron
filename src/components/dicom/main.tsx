90f55196cfea992dd0d2456760b721f09bff4417
Vlad Barbu <vladbarbu14@yahoo.com>
2024-07-21 23:57:24 +0300

    return full info dict

--- src/components/dicom/main.tsx
+++ src/components/dicom/main.tsx
@@ -1,5 +1,5 @@
 import dicomParser from "dicom-parser";
-import { uids } from "./uids";
+
 export const getDicomTags = async (filePath: string, fileName: string) => {
     console.log("filePath", filePath);
     console.log("fileName", fileName);
@@ -11,55 +11,90 @@ export const getDicomTags = async (filePath: string, fileName: string) => {
         const buffer = await fs.readFile(path.join(filePath, fileName));
         const byteArray = new Uint8Array(buffer);
         const dataSet = dicomParser.parseDicom(byteArray);
-        var patientName = dataSet.string("x00100010");
-        console.log("Patient Name = " + patientName);
 
-        var patientId = dataSet.string("x00100020");
-        console.log("Patient ID = " + patientId);
+        const sections: { [key: string]: { [key: string]: string } } = {
+            "Patient Information": {
+                x00100010: "Patient Name",
+                x00100020: "Patient ID",
+                x00100030: "Patient Birth Date",
+                x00100040: "Patient Sex",
+            },
+            "Study Information": {
+                x00081030: "Study Description",
+                x00181030: "Protocol Name",
+                x00080050: "Accession #",
+                x00200010: "Study Id",
+                x00080020: "Study Date",
+                x00080030: "Study Time",
+            },
+            "Series Information": {
+                x0008103e: "Series Description",
+                x00200011: "Series #",
+                x00080060: "Modality",
+                x00180015: "Body Part",
+                x00080021: "Series Date",
+                x00080031: "Series Time",
+            },
+            "Instance Information": {
+                x00200013: "Instance #",
+                x00200012: "Acquisition #",
+                x00080022: "Acquisition Date",
+                x00080032: "Acquisition Time",
+                x00080023: "Content Date",
+                x00080033: "Content Time",
+            },
+            "Image Information": {
+                x00280010: "Rows",
+                x00280011: "Columns",
+                x00280004: "Photometric Interpretation",
+                x00080008: "Image Type",
+                x00280100: "Bits Allocated",
+                x00280101: "Bits Stored",
+                x00280102: "HighBit",
+                x00280103: "Pixel Representation",
+                x00281053: "Rescale Slope",
+                x00281052: "Rescale Intercept",
+                x00200032: "Image Position Patient",
+                x00200037: "Image Orientation Patient",
+                x00280030: "Pixel Spacing",
+                x00280002: "Samples Per Pixel",
+            },
+            "Equipment Information": {
+                x00080070: "Manufacturer",
+                x00081090: "Model",
+                x00081010: "Station Name",
+                x00020016: "AE Title",
+                x00080080: "Institution Name",
+                x00181020: "Software Version",
+                x00020013: "Implementation Version Name",
+            },
+            UIDS: {
+                x0020000d: "Study UID",
+                x0020000e: "Series UID",
+                x00080018: "Instance UID",
+                x00080016: "SOP Class UID",
+                x00020010: "Transfer Syntax UID",
+                x00200052: "Frame of Reference UID",
+            },
+        };
 
-        var patientBirthDate = dataSet.string("x00100030");
-        console.log("Patient Birth Date = " + patientBirthDate);
+        const result: { [key: string]: { [key: string]: string } } = {};
 
-        var studyDescription = dataSet.string("x00081030");
-        console.log("Study Description = " + studyDescription);
-
-        var protocolName = dataSet.string("x00181030");
-        console.log("Protocol Name = " + protocolName);
-
-        var accessionNumber = dataSet.string("x00080050");
-        console.log("Accession # = " + accessionNumber);
-
-        var studyId = dataSet.string("x00200010");
-        console.log("Study Id = " + studyId);
-
-        var studyDate = dataSet.string("x00080020");
-        console.log("Study Date = " + studyDate);
-
-        var studyTime = dataSet.string("x00080030");
-        console.log("Study Time = " + studyTime);
-
-        var seriesDescription = dataSet.string("x0008103e");
-        console.log("Series Description = " + seriesDescription);
-
-        var seriesDate = dataSet.string("x00080021");
-        console.log("Series Date = " + seriesDate);
-
-        var seriesTime = dataSet.string("x00080031");
-        console.log("Series Time = " + seriesTime);
-
-        var acquisitionDate = dataSet.string("x00080022");
-        console.log("Acquisition Date = " + acquisitionDate);
-
-        var acquisitionTime = dataSet.string("x00080032");
-        console.log("Acquisition Time = " + acquisitionTime);
-
-        var contentDate = dataSet.string("x00080023");
-        console.log("Content Date = " + contentDate);
-
-        var contentTime = dataSet.string("x00080033");
-        console.log("Content Time = " + contentTime);
-
-        return 200;
+        for (const section in sections) {
+            if (Object.prototype.hasOwnProperty.call(sections, section)) {
+                result[section] = {};
+                for (const tag in sections[section]) {
+                    if (Object.prototype.hasOwnProperty.call(sections[section], tag)) {
+                        const value = dataSet.string(tag);
+                        if (value !== undefined) {
+                            result[section][sections[section][tag]] = value;
+                        }
+                    }
+                }
+            }
+        }
+        console.log(result);
+        return result;
     } catch (error) {
         console.error("Error reading or parsing DICOM file:", error);
         throw error;
