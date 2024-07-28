a01630a7ad6996ff39befdf982346fa581ccbec9
Vlad Barbu <vladbarbu14@yahoo.com>
2024-07-28 20:38:25 +0300

    add loading overlay

--- src/pages/DirectoryPage.tsx
+++ src/pages/DirectoryPage.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import LangToggle from "@/components/LangToggle";
 import { FolderPlus } from "lucide-react";
-
+import DirectoryLoadingOverlay from "@/components/DirectoryLoadingOverlay";
 export default function DirectoryPage({
     isLoading,
     setIsLoading,
@@ -12,7 +12,6 @@ export default function DirectoryPage({
 }) {
     const { t } = useTranslation();
     const [outputDirectory, setOutputDirectory] = useState<string | null>(null);
-    console.log(isLoading);
 
     const getDirectoryContent = async (srcDir: string) => {
         const path = window.electron.path;
@@ -47,18 +46,15 @@ export default function DirectoryPage({
 
     return (
         <div className="relative  flex  h-full w-full flex-col rounded-xl border-2 border-dashed border-secondary p-4 lg:col-span-2">
-            {isLoading ? (
-                "Loading..."
-            ) : (
-                <div
-                    className="flex h-full flex-col items-center justify-center gap-4"
-                    onClick={handleDirectoryPicker}
-                >
-                    <div className="flex flex-col items-center justify-center gap-4">
-                        <FolderPlus className="mr-2 h-8 w-8" /> Search for one or more files
-                    </div>
+            <div
+                className="flex h-full flex-col items-center justify-center gap-4"
+                onClick={handleDirectoryPicker}
+            >
+                <div className="flex flex-col items-center justify-center gap-4">
+                    <FolderPlus className="mr-2 h-8 w-8" /> Search for one or more files
                 </div>
-            )}
+            </div>
+            <DirectoryLoadingOverlay isOpen={isLoading} />
         </div>
     );
 }
